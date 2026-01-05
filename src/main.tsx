1b2dd742ee5a24c793e2fba7ee2529ad9f7a9474
Fábio Henrique <psifabiohenrique@outlook.com>
2026-01-05 11:53:06 -0300

    Removed app.css and updated global css with attributes of app.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import "./styles/themes.css";
 import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
