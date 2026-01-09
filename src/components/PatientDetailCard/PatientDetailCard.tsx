92055adf3b9938e54ec535bffe5b9d27b0b1751b
Fábio Henrique <psifabiohenrique@outlook.com>
2026-01-09 16:49:57 -0300

    Created patient detail card
    Updated patient detail page to use patient detail card and newers cards

---
+++ src/components/PatientDetailCard/PatientDetailCard.tsx
@@ -0,0 +1,64 @@
+import Button from "../Button/Button";
+import { useNavigate } from "react-router-dom";
+import type { Patient } from "../../types/patient";
+import { formatPhoneNumberIntl } from "react-phone-number-input";
+import {
+  formatDate,
+  formatTime,
+  translateWeekday,
+  translateGender,
+} from "../../utils/formatters";
+import styles from "./PatientDetailCard.module.css";
+
+interface PatientDetailCardProps {
+  patient: Patient;
+  uuid: string;
+}
+
+export function PatientDetailCard({ patient, uuid }: PatientDetailCardProps) {
+  const navigate = useNavigate();
+
+  function handlePhoneClick() {
+    navigate(`https://wa.me/${patient.patient.phone}`);
+  }
+  return (
+    <div className={styles.patientDetailCard}>
+      <h1>Detalhes do Paciente</h1>
+      <Button onClick={() => navigate(`/patient/${uuid}/edit`)}>Editar</Button>
+
+      <section>
+        <h3>Dados Pessoais</h3>
+        <p>
+          <strong>Nome:</strong> {patient.patient.full_name}
+        </p>
+        <p>
+          <strong>E-mail:</strong> {patient.patient.email}
+        </p>
+        <p onClick={handlePhoneClick}>
+          <strong>Telefone:</strong>{" "}
+          {patient.patient.phone
+            ? formatPhoneNumberIntl(patient.patient.phone)
+            : "N/A"}
+        </p>
+        <p>
+          <strong>Data de Nascimento:</strong>{" "}
+          {formatDate(patient.patient.birth_date)}
+        </p>
+        <p>
+          <strong>Gênero:</strong> {translateGender(patient.patient.gender)}
+        </p>
+      </section>
+
+      <section>
+        <h3>Dados do Tratamento</h3>
+        <p>
+          <strong>Dia da Semana:</strong> {translateWeekday(patient.weekday)}
+        </p>
+        <p>
+          <strong>Horário:</strong> {formatTime(patient.start_time)} -{" "}
+          {formatTime(patient.end_time)}
+        </p>
+      </section>
+    </div>
+  );
+}
--- src/pages/Dashboard/patients/PatientDetailPage.tsx
+++ src/pages/Dashboard/patients/PatientDetailPage.tsx
@@ -1,21 +1,15 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { formatPhoneNumberIntl } from "react-phone-number-input";
+import { useParams } from "react-router-dom";
 import { getPatient } from "../../../api/patient.service";
 import type { Patient } from "../../../types/patient";
-import {
-  formatDate,
-  formatTime,
-  translateWeekday,
-  translateGender,
-} from "../../../utils/formatters";
-import Button from "../../../components/Button/Button";
+import { PatientDetailCard } from "../../../components/PatientDetailCard/PatientDetailCard";
+import { RecordList } from "../../../components/RecordList/RecordList";
+import styles from "./PatientDetailPage.module.css";
 
 export function PatientDetailPage() {
   const { uuid } = useParams<{ uuid: string }>();
   const [patient, setPatient] = useState<Patient | null>(null);
   const [isLoading, setIsLoading] = useState(true);
-  const navigate = useNavigate();
 
   useEffect(() => {
     if (uuid) {
@@ -29,43 +23,9 @@ export function PatientDetailPage() {
   if (!patient) return <div>Paciente não encontrado.</div>;
 
   return (
-    <div>
-      <h1>Detalhes do Paciente</h1>
-      <Button onClick={() => navigate(`/patient/${uuid}/edit`)}>Editar</Button>
-
-      <section>
-        <h3>Dados Pessoais</h3>
-        <p>
-          <strong>Nome:</strong> {patient.patient.full_name}
-        </p>
-        <p>
-          <strong>E-mail:</strong> {patient.patient.email}
-        </p>
-        <p>
-          <strong>Telefone:</strong>{" "}
-          {patient.patient.phone
-            ? formatPhoneNumberIntl(patient.patient.phone)
-            : "N/A"}
-        </p>
-        <p>
-          <strong>Data de Nascimento:</strong>{" "}
-          {formatDate(patient.patient.birth_date)}
-        </p>
-        <p>
-          <strong>Gênero:</strong> {translateGender(patient.patient.gender)}
-        </p>
-      </section>
-
-      <section>
-        <h3>Dados do Tratamento</h3>
-        <p>
-          <strong>Dia da Semana:</strong> {translateWeekday(patient.weekday)}
-        </p>
-        <p>
-          <strong>Horário:</strong> {formatTime(patient.start_time)} -{" "}
-          {formatTime(patient.end_time)}
-        </p>
-      </section>
+    <div className={styles.patientDetailPage}>
+      <PatientDetailCard patient={patient} uuid={uuid!} />
+      <RecordList />
     </div>
   );
 }
