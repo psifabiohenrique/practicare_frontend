0d23d426510f0db61550496904184fe56dbf07b2
Fábio Henrique <psifabiohenrique@outlook.com>
2026-01-09 20:29:59 -0300

    Created report types. Created report services.

---
+++ src/api/report.service.ts
@@ -0,0 +1,39 @@
+import { api } from "./axios";
+import type {
+  Report,
+  ReportPayload,
+  ReportUpdatePayload,
+  ReportListParams,
+} from "../types/report";
+
+export async function listReports(
+  treatment_uuid: string,
+  params: ReportListParams
+) {
+  const response = await api.get<Report[]>(
+    `/treatment-reports/treatment/${treatment_uuid}/`,
+    { params }
+  );
+  return response.data;
+}
+
+export async function getReport(report_uuid: string): Promise<Report> {
+  const response = await api.get(`/treatment-reports/${report_uuid}/`);
+  return response.data;
+}
+
+export async function createReport(payload: ReportPayload): Promise<Report> {
+  const response = await api.post(`/treatment-reports/`, payload);
+  return response.data;
+}
+
+export async function updateReport(
+  report_uuid: string,
+  payload: ReportUpdatePayload
+): Promise<Report> {
+  const response = await api.patch(
+    `/treatment-reports/${report_uuid}/`,
+    payload
+  );
+  return response.data;
+}
---
+++ src/types/report.ts
@@ -0,0 +1,34 @@
+export interface Report {
+  uuid: string;
+  treatment_uuid: string;
+  demand_description: string;
+  procedures: string;
+  analysis: string;
+  conclusion: string;
+  issue_date: string;
+  start_date_period: string;
+  end_date_period: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export type ReportPayload = Pick<
+  Report,
+  | "treatment_uuid"
+  | "demand_description"
+  | "procedures"
+  | "analysis"
+  | "conclusion"
+  | "issue_date"
+  | "start_date_period"
+  | "end_date_period"
+>;
+
+export type ReportUpdatePayload = Partial<
+  Omit<ReportPayload, "treatment_uuid" | "created_at" | "updated_at">
+>;
+
+export type ReportListParams = {
+  skip?: number;
+  limit?: number;
+};
