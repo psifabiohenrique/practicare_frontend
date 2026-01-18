cc04b2fe8d9cb70d9166afba97a3d0c9080d2143
Fábio Henrique <psifabiohenrique@outlook.com>
2026-01-18 18:43:02 -0300

    Corrected patient create page call to patient form

--- src/pages/Dashboard/patients/PatientCreatePage.tsx
+++ src/pages/Dashboard/patients/PatientCreatePage.tsx
@@ -1,15 +1,10 @@
 import { PatientForm } from "../../../components/PatientForm/PatientForm";
-import { createPatient } from "../../../api/patient.service";
 
 export function PatientCreatePage() {
   return (
     <div style={{ width: "100%" }}>
       <h1>Criar Paciente</h1>
-      <PatientForm
-        onSubmit={createPatient}
-        submitButtonText="Cadastrar Paciente"
-        successMessage="Paciente cadastrado com sucesso!"
-      />
+      <PatientForm />
     </div>
   );
 }
